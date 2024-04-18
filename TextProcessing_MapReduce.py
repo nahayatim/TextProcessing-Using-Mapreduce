03389c04ca45d2d1cb0e3420a7f6b16ea8e54510
Mahtab Nahayati <mahtabnahayati@gmail.com>
2024-04-18 21:23:52 +0200

    calcualting the chi-Square- ordering the terms-merging the list

--- TextProcessing_MapReduce.py
+++ TextProcessing_MapReduce.py
@@ -1,6 +1,7 @@
 import re
 import json
 from mrjob.job import MRJob
+from mrjob.step import MRStep
 import collections
 
 STOPWORDS = set() #We will populate this with stop words
@@ -15,6 +16,16 @@ class ChiSquareWordCount(MRJob):
     def configure_args(self):
         super(ChiSquareWordCount, self).configure_args()
         self.add_file_arg('--stopwords')
+
+    def steps(self):
+        return [
+            MRStep(mapper_init=self.mapper_init,
+                   mapper=self.mapper,
+                   reducer=self.reducer),
+            MRStep(reducer=self.second_reducer),
+            MRStep(reducer=self.third_reducer),
+            MRStep(reducer=self.fourth_reducer)
+        ]
 
     def mapper_init(self):
         load_stopwords(self.options.stopwords)
@@ -31,27 +42,36 @@ class ChiSquareWordCount(MRJob):
 
         for token in filtered_tokens:
             yield (token, category), 1 
+            yield ('*total', 1)
+            yield ((token, '*total'), 1)
+            yield ((category, '*total'), 1)
 
     def reducer(self, key, values):
-        word, category = key
-        word_count = sum(values)  # Count of the word within the category
+       yield key, sum(values)
 
-        # Data structure to store counts
-        self.counts = collections.defaultdict(lambda: {
-            'word_count': 0,
-            'category_count': 0,
-            'overall_count': 0
-        })  
+        
 
-        # Update counts
-        self.counts[word]['word_count'] += word_count
-        self.counts[word]['category_count'] += 1  # Count occurrences of this category
-        self.counts[word]['overall_count'] += word_count 
+    def second_reducer(self, key, values):
+        total = sum(values)
+        if key[1] == '*total':
+            self.increment_counter('total', key[0], total)
+        else:
+            chi_square = (total - self.get_counter('total', key[0]))**2 / self.get_counter('total', key[0])
+            yield (key[1], -chi_square), key[0]
 
-    def final(self):
-        """Emit aggregated counts for chi-square calculations"""
-        for word, data in self.counts.items():
-            yield word, str(data)
+    
+    def third_reducer(self, key, values):
+        terms = list(values)[:75]
+        for term in terms:
+            yield None, term
+        yield key[0], terms
+
+    def fourth_reducer(self, key, values):
+        if key is None:
+           yield '*merged', ' '.join(sorted(set(values)))
+        else:
+            yield key, ' '.join(f'{term}:{chi_square}' for term, chi_square in values)
+
 
 if __name__ == '__main__':
     ChiSquareWordCount.run()
